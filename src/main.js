3d70e52eb583b52cb423d98b7f30adcf6bda1f3d
Nick <nick@fps-game.dev>
2026-02-04 19:54:50 +0000

    Reduce blood droplet size even further to 0.04

--- src/main.js
+++ src/main.js
@@ -925,7 +925,7 @@ const bulletMaterial = new THREE.MeshBasicMaterial({
 
 // Blood particles
 const bloodParticles = [];
-const bloodGeometry = new THREE.SphereGeometry(0.08, 4, 4); // Smaller droplets
+const bloodGeometry = new THREE.SphereGeometry(0.04, 4, 4); // Even smaller droplets
 
 function createBloodSplatter(position) {
   // Create 8-12 blood particles
